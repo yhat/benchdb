c5732fcd3962ef1a0c190b71b827adb072f76b31
Eric Cox <emcox84@gmail.com>
2015-06-29 18:01:38 -0400

    adding usage and exit func and some constants

--- gobenchdb.go
+++ gobenchdb.go
@@ -16,12 +16,26 @@ var (
 		"run only those benchmarks matching the regular expression")
 )
 
+const Postgres = "postgres"
+
 var usage = `Usage: gobenchdb [options...]
 
 Options:
   -conn        postgres database connection string
   -table       postgres table name
   -test.bench  run only those benchmarks matching the regular expression`
+
+func printUsageExit(message string) {
+	// inspired by rakyll/boom
+	if message != "" {
+		fmt.Fprintf(os.Stderr, message)
+		fmt.Fprintf(os.Stderr, "\n\n")
+
+	}
+	flag.Usage()
+	fmt.Fprintf(os.Stderr, "\n")
+	os.Exit(1)
+}
 
 func main() {
 	flag.Usage = func() {
@@ -30,20 +44,29 @@ func main() {
 
 	// Parse command line args
 	flag.Parse()
+	c := *conn
+	t := *table
+	tregex := *testBench
+
+	if c == "" {
+		printUsageExit("database conn must be specified")
+	}
+	if t == "" {
+		printUsageExit("database table must be specified")
+	}
 
 	// write data in csv format to stdout
 	// TODO: convert this to write to a sql db
 	writer := csv.NewWriter(os.Stdout)
 	defer writer.Flush()
 	_, err := (&benchdb.BenchDB{
-		Regex:     *testBench,
-		Driver:    "postgres",
-		ConnStr:   *conn,
-		TableName: *table,
+		Regex:     tregex,
+		Driver:    Postgres,
+		ConnStr:   c,
+		TableName: t,
 		CsvWriter: *writer,
 	}).Run()
 	if err != nil {
-		flag.Usage()
-		os.Exit(2)
+		printUsageExit(err.Error())
 	}
 }
