cad474e162627e86913c1ea016176152c8cba90b
Eric Cox <emcox84@gmail.com>
2015-06-30 11:33:51 -0400

    adding better docstrings, and moving all db logic to WriteBenchSet.

--- benchdb/benchdb.go
+++ benchdb/benchdb.go
@@ -15,7 +15,7 @@ import (
 	"golang.org/x/tools/benchmark/parse"
 )
 
-// DB represents a sql database that can be used to write
+// BenchDB represents a postgresql database that can be used to write
 // benchmark data to.
 type BenchDB struct {
 	Regex     string // regex used to run benchmark sets
@@ -26,9 +26,17 @@ type BenchDB struct {
 	dbConn *sql.DB
 }
 
-// WriteBenchSet writes data from a parsed benchmark set to a DB table and a csv.Writer
-// It returns the number of benchmark tests written and any error encountered.
+// WriteBenchSet is responsible for managing the postgres sql connection and writing
+// a parsed benchSet to a postrges table. It closes the postgres sql connection,
+// returns the number of benchmark tests written, and any error encountered.
 func (benchdb *BenchDB) WriteBenchSet(benchSet parse.Set) (int, error) {
+	sqlDB, err := sql.Open(benchdb.Driver, benchdb.ConnStr)
+	if err != nil {
+		return 0, fmt.Errorf("could not connect to db: %v", err)
+	}
+	defer sqlDB.Close()
+	benchdb.dbConn = sqlDB
+
 	cnt := 0
 	for _, b := range benchSet {
 		n := len(b)
@@ -66,25 +74,19 @@ func saveBenchmark(dbConn *sql.DB, table string, b parse.Benchmark) error {
 	return tx.Commit()
 }
 
-// Run executes all of the go test benchmarks that match regexpr in the
-// current directory. By default it does not run unit tests. It returns
-// parsed benchmark stats in a parse.Set and returns any error encountered.
+// Run runs all of the go test benchmarks that match regexpr in the
+// current directory. By default it does not run unit tests by way of setting
+// test.run to XXX. It is also responsible for parsing the benchmark set and calls
+// WriteBenchSet to write the benchmark data to a postgresql database. It returns
+// any error encountered.
 func (benchdb *BenchDB) Run() (int, error) {
-	// Connect to a sql database.
-	sqlDB, err := sql.Open(benchdb.Driver, benchdb.ConnStr)
-	if err != nil {
-		return 0, fmt.Errorf("could not connect to db: %v", err)
-	}
-	defer sqlDB.Close()
-	benchdb.dbConn = sqlDB
-
-	// Runs exec a subprocess for go test bench command and write
+	// Exec a subprocess for go test bench command and write
 	// to both stdout and a byte buffer.
 	cmd := exec.Command("go", "test", "-bench", benchdb.Regex,
 		"-test.run", "XXX", "-benchmem")
 	var out bytes.Buffer
 	cmd.Stdout = io.MultiWriter(os.Stdout, &out)
-	err = cmd.Run()
+	err := cmd.Run()
 	if err != nil {
 		return 0, fmt.Errorf("command failed: %v", err)
 	}
