bb6a7e21b2a5f3018352e6f5400c281ce914049e
Eric Cox <emcox84@gmail.com>
2015-07-02 13:49:29 -0400

    docs

--- gobenchdb.go
+++ gobenchdb.go
@@ -1,4 +1,5 @@
-/* gobenchdb is a command line tool for running and storing go benchmark data in a
+/*
+gobenchdb is a command line tool for running and storing go benchmark data in a
 database. It runs the go test -bench command in the current working directory
 and parses the output using the parse package. The parsed data is then written to
 sql database of your choice.
